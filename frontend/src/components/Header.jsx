f5aef2b3c9b4f1e9501ae87ac6c94692cc377937
Felipe Arce <arceprogramando@gmail.com>
2024-03-13 16:36:04 -0300

    feat(frontend): :hammer: Init Header with h1

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <>
+      <header className="bg-black text-white text-center text-2xl p-3">
+        <h1>Prueba Tecnica Middas</h1>
+      </header>
+    </>
+  );
+};
