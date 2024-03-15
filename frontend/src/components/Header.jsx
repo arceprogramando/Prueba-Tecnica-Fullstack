311b5251161f0e942f27494d89595da0e05ed099
Felipe Arce <arceprogramando@gmail.com>
2024-03-15 01:10:20 -0300

    feat(frontend): :lipstick: Added Margin to Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <>
-      <header className="bg-black text-white text-center text-2xl p-3">
+      <header className="bg-black text-white text-center text-2xl p-3 mb-8">
         <h1>Prueba Tecnica Middas</h1>
       </header>
     </>
