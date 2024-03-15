ebb6ffe3039812fc289269e8f6cb332e5374f03e
Felipe Arce <arceprogramando@gmail.com>
2024-03-15 03:26:46 -0300

    feat(frontend): :lipstick: added margin bottom

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <>
-      <header className="bg-black text-white text-2xl p-4 text-center uppercase">
+      <header className="bg-black text-white text-2xl p-4 text-center uppercase mb-8">
         <h1>Prueba Tecnica Middas</h1>
       </header>
     </>
