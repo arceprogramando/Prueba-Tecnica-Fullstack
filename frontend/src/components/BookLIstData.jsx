9a6cf4772a722a960a379bd629e52e9e85fe09a1
Felipe Arce <arceprogramando@gmail.com>
2024-03-15 11:59:34 -0300

    feat(frontend): :recycle: refactor and join with form component

--- frontend/src/components/BookLIstData.jsx
+++ frontend/src/components/BookLIstData.jsx
@@ -1,34 +1,9 @@
-import { useEffect, useState } from 'react';
 import { BookList } from './BookList';
-import { Loading } from '../UI/Loading';
-import { getBookRequest } from '../../api/tasks.api';
 
-export const BookListData = () => {
-  const [bookData, setBookData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const getBooksData = await getBookRequest();
-        setBookData(getBooksData);
-      } catch (error) {
-        setError('Error al traer la data');
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <Loading />;
+export const BookListData = ({ books }) => {
+  if (!books || books.length === 0) {
+    return <p>No hay elementos agregados</p>;
   }
 
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  return <div>{bookData?.length > 0 ? <BookList books={bookData} /> : <p>No hay elementos agregados</p>}</div>;
+  return <BookList books={books} />;
 };
