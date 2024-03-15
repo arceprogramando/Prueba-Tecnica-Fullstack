05450b2f481970cd4386e96d8badfad1a218ebc0
Felipe Arce <arceprogramando@gmail.com>
2024-03-15 02:51:26 -0300

    refactor(frontend): :recycle: Refactor code

--- frontend/src/components/BookItem.jsx
+++ frontend/src/components/BookItem.jsx
@@ -1,12 +1,12 @@
 export const BookItem = ({ data }) => {
+  const { title, author, category, publicationDate } = data;
+
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 mb-4">
-      <div className="text-gray-800">
-        {data.title ? <h2 className="text-lg font-semibold mb-2">{data.title}</h2> : <p>Falto agregar titulo</p>}
-        {data.author ? <p className="text-sm">{data.author}</p> : <p>Falto agregar autor</p>}
-        {data.category ? <p className="text-xs text-gray-600 mt-2">{data.category}</p> : <p>Falto agregar categoria</p>}
-        {data.publicationDate ? <p>Fecha de Publicacion: {data.publicationDate}</p> : <p>Falto agregar la fecha de Publicación</p>}
-      </div>
+    <div className="bg-white rounded-lg shadow-md p-4 mb-4 text-gray-800">
+      <h2 className="text-lg font-semibold mb-2">{title || 'Falto agregar titulo'}</h2>
+      <p className="text-sm">{author || 'Falto agregar autor'}</p>
+      <p className="text-xs text-gray-600 mt-2">{category || 'Falto agregar categoria'}</p>
+      <p>{publicationDate || 'Falto agregar la fecha de Publicación'}</p>
     </div>
   );
 };
